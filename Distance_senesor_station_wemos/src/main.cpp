deb4ab77cee82af84cbfba33489001df4688b29f
YigalB1 <yigalb@hotmail.com>
2020-12-23 17:44:25 +0200

    working 1

--- Distance_senesor_station_wemos/src/main.cpp
+++ Distance_senesor_station_wemos/src/main.cpp
@@ -3,38 +3,56 @@
 
 int servo_pin = 12; // D6
 
-int echoPin = 7; // Echo Pin
-int trigPin = 6; // Trigger Pin
+int echoPin = 13; // Echo Pin (D7)
+int trigPin = 14; // Trigger Pin (D5) 
 int pos = 0;    // variable to store the servo position
  
 Servo servo_base;  // create servo object to control a servo
 
 int ultrasinic_measure();
+int move_servo_and_read(int _pos);
+void radar_search(int min_dist, int _max_dist);
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   servo_base.attach(servo_pin);  // attaches the servo on pin 9 
   Serial.begin(9600);
   
 } // of setup
 
 void loop() {
-   digitalWrite(LED_BUILTIN, HIGH);
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    servo_base.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-   digitalWrite(LED_BUILTIN, LOW);
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    servo_base.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  int dist = ultrasinic_measure();
-  Serial.print("Distance is: ");
-  Serial.println(dist);
-
+  digitalWrite(LED_BUILTIN, HIGH); // led OFF
+  radar_search(20, 40); // TBD - use min & max to define a range of detection
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW); // led ON
 } // of loop
+
+
+int move_servo_and_read(int _pos) {
+  servo_base.write(_pos);
+  delay(15); // waits 15ms for the servo to reach the position
+  return ultrasinic_measure();  
+}
+
+void radar_search(int min_dist, int _max_dist) {
+   for (pos = 0; pos <= 180; pos += 1) { // in degrees, 1-180
+    int dist = move_servo_and_read(pos);
+    Serial.print("Distance is: ");
+    Serial.println(dist);
+    delay(250);   
+    }
+  
+    for (pos = 180; pos >=0 ; pos -= 1) { // in degrees, 1-180
+    int dist = move_servo_and_read(pos);
+    Serial.print("Distance is: ");
+    Serial.println(dist);
+    delay(250);   
+    }
+
+
+}
 
 int ultrasinic_measure() {
   long duration, distance; // Duration used to calculate distance
@@ -46,9 +64,8 @@ int ultrasinic_measure() {
   duration = pulseIn(echoPin, HIGH);
   //Calculate the distance (in cm) based on the speed of sound.
   distance = duration/58.2;
-  Serial.println(distance);
+  //Serial.println(distance);
   //Delay 50ms before next reading.
   delay(50);
   return distance;
 }
-
